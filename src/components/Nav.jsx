1e8c87d9fadb892f0a96548800be09f9ce72b572
bakmaiev <bakmaiev@gmail.com>
2023-12-16 19:42:52 +0200

    Small fix href

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,7 +9,7 @@ const Nav = ({ isOpen, onClick }) => {
   return (
     <header className="padding-x py-8 absolute z-10 w-full">
       <nav className="flex justify-between items-center max-container">
-        <a href="/">
+        <a href="/nike">
           <img src={headerLogo} alt="Logo" width={130} height={29} />
         </a>
         <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
